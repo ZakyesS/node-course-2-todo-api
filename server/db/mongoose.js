58a23639187f61740bb32f38340a4654439019fe
Yuneysa S <ysuarez@20h.es>
2018-04-21 00:59:47 +0100

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,4 +3,4 @@ let mongoose = require('mongoose');
 mongoose.Promise = global.Promise; //para que el mongoose use las promises.
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
-module.exports = { mongoose};
+module.exports = {mongoose};
